6ab1275218fe4c6066fe1e1b0809b7b4b2519734
Chambrse <shaneechambry@gmail.com>
2018-05-15 08:58:22 -0700

    game updates and font change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,18 @@
 function hangman() {
+
+    /* Choose the first word */
+    var wordlist = ["Arryn", "Baratheon", "Greyjoy", "Lannister", "Martell", "Mormont", "Stark", "Targaryen", "Tully", "Tyrell"];
+    newWord = wordlist[Math.floor(Math.random() * wordlist.length)];
+
+    /* initialize the game board with the appropriate length */
+    var dashString = [];
+    for (var i = 0; i < newWord.length; i++) {
+        dashString = dashString + " _";
+        console.log(dashString);
+    }
+    
+    document.getElementById("word").innerHTML = dashString;
     document.onkeyup = function(event) {
         console.log("test");
     }
-}
+};
