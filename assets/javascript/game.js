9cb59dd937586fe10b8ad358f924ac1c733a5a73
Chambrse <shaneechambry@gmail.com>
2018-05-17 10:13:32 -0700

    added sound, updated notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,21 +25,30 @@ function hangman() {
     }
     document.getElementById("word").innerHTML = dashString;
 
+    var winSound = new Audio("\assets\sounds\165491__chripei__victory-cry-reverb-2.wav");
+    
 
 
     document.onkeyup = function (event) {
+
+        /* If it hasn't already been guessed, and it isn't in the word, dock a guess. */
         if (newWord.includes(event.key) == false && letters.includes(event.key) == false) {
             guesses--;
         }
 
+        /* If it hasn't been guessed, add it to the list of guessed words. */
         if (letters.includes(event.key) == false) {
             letters.push(event.key);
         }
-        console.log(letters.includes(event.key));
 
+/*         console.log(letters.includes(event.key));
+ */
+
+        /* get index of first occurence of the input in the word. */
         var index = 0;
         index = newWord.indexOf(event.key, index);
 
+        /* if you find another occurence, replace that dash with its corresponding letter. Then, look for another occurence. Until there are no more occurences. */
         while (index != -1) {
             console.log('index1', index);
             dashString = dashString.substr(0, index) + newWord[index] + dashString.substr(index + 1);
@@ -48,6 +57,8 @@ function hangman() {
             console.log('index2', index);
         }
 
+        /* If the word is correctly guessed and you haven't run out of guesses, you win!
+            Add a win, reset the game, change the banner */
         if (dashString.includes("_") == false && guesses > 0) {
 
             wins++;
@@ -63,6 +74,7 @@ function hangman() {
 
             document.getElementById("houseBanner").setAttribute("src", path);
 
+            /*  if you run out of guesses, you lose. reset*/
         } else if (guesses == 0) {
             wins = 0;
             guesses = 10;
@@ -76,6 +88,7 @@ function hangman() {
             }
         }
 
+        /* Update game board. */
         document.getElementById("wins").innerHTML = "Wins: " + wins;
         document.getElementById("guesses").innerHTML = "Guesses Remaining: " + guesses;
         document.getElementById("word").innerHTML = dashString;
