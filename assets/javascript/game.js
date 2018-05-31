2db47af0b7758e4cbb1eccc3f990e24def28cd5f
Chambrse <shaneechambry@gmail.com>
2018-05-31 10:58:50 -0700

    multiple guess check and letter check

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ function hangman() {
 
     /* Choose the first word */
     var wordlist = ["arryn", "baratheon", "greyjoy", "lannister", "martell", "mormont", "stark", "targaryen", "tully", "tyrell"];
-    
+
     wordnumber = Math.floor(Math.random() * wordlist.length);
     newWord = wordlist[wordnumber];
 
@@ -30,78 +30,83 @@ function hangman() {
     document.getElementById("word").innerHTML = dashString;
 
     var winSound = new Audio("assets/sounds/165491__chripei__victory-cry-reverb-2.wav");
-    
+
 
 
     document.onkeyup = function (event) {
 
-        /* If it hasn't already been guessed, and it isn't in the word, dock a guess. */
-        if (newWord.includes(event.key) == false && letters.includes(event.key) == false) {
-            guesses--;
-        }
+        /* make sure its a letter */
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
 
-        /* If it hasn't been guessed, add it to the list of guessed words. */
-        if (letters.includes(event.key) == false) {
-            letters.push(event.key);
-        }
-
-/*         console.log(letters.includes(event.key));
- */
-
-        /* get index of first occurence of the input in the word. */
-        var index = 0;
-        index = newWord.indexOf(event.key, index);
-
-        /* if you find another occurence, replace that dash with its corresponding letter. Then, look for another occurence. Until there are no more occurences. */
-        while (index != -1) {
-            dashString = dashString.substr(0, index) + newWord[index] + dashString.substr(index + 1);
-            index = newWord.indexOf(event.key, index + 1);
-        }
-
-        /* If the word is correctly guessed and you haven't run out of guesses, you win!
-            Add a win, reset the game, change the banner */
-        if (dashString.includes("_") == false && guesses > 0) {
-
-            wins++;
-            guesses = 10;
-            letters = [];
-
-            winSound.play();
-            document.getElementById("houseBanner").setAttribute("src", path);
-
-            wordnumber = Math.floor(Math.random() * wordlist.length);
-            newWord = wordlist[wordnumber];
-            console.log(newWord);
-            path = pathlist[wordnumber];
-            clue = cluelist[wordnumber];
-
-            dashString = "";
-            for (var i = 0; i < newWord.length; i++) {
-                dashString = dashString + "_";
+            /* If it hasn't already been guessed, and it isn't in the word, dock a guess. */
+            if (newWord.includes(event.key) == false && letters.includes(event.key) == false) {
+                guesses--;
             }
 
-            /*  if you run out of guesses, you lose. reset*/
-        } else if (guesses == 0) {
-            wins = 0;
-            guesses = 10;
-            letters = [];
-
-            wordnumber = Math.floor(Math.random() * wordlist.length);
-            newWord = wordlist[wordnumber];
-            path = pathlist[wordnumber];
-            clue = cluelist[wordnumber];
-
-            dashString = "";
-            for (var i = 0; i < newWord.length; i++) {
-                dashString = dashString + "_";
+            /* If it hasn't been guessed, add it to the list of guessed words. */
+            if (letters.includes(event.key) == false) {
+                letters.push(event.key);
+            } 
+            /* if you've guessed this letter, complain */
+            else if (letters.includes(event.key)) {
+                $("#toolTip").text("You've already guessed that letter!");
             }
-        }
 
-        /* Update game board. */
-        document.getElementById("clue").innerHTML = "Clue: " + clue;
-        document.getElementById("wins").innerHTML = "Wins: " + wins;
-        document.getElementById("guesses").innerHTML = "Guesses Remaining: " + guesses;
-        document.getElementById("word").innerHTML = dashString;
-        document.getElementById("letters").innerHTML = "Letters guessed: " + letters;
+            /* get index of first occurence of the input in the word. */
+            var index = 0;
+            index = newWord.indexOf(event.key, index);
+
+            /* if you find another occurence, replace that dash with its corresponding letter. Then, look for another occurence. Until there are no more occurences. */
+            while (index != -1) {
+                dashString = dashString.substr(0, index) + newWord[index] + dashString.substr(index + 1);
+                index = newWord.indexOf(event.key, index + 1);
+            }
+
+            /* If the word is correctly guessed and you haven't run out of guesses, you win!
+                Add a win, reset the game, change the banner */
+            if (dashString.includes("_") == false && guesses > 0) {
+
+                wins++;
+                guesses = 10;
+                letters = [];
+
+                winSound.play();
+                document.getElementById("houseBanner").setAttribute("src", path);
+
+                wordnumber = Math.floor(Math.random() * wordlist.length);
+                newWord = wordlist[wordnumber];
+                console.log(newWord);
+                path = pathlist[wordnumber];
+                clue = cluelist[wordnumber];
+
+                dashString = "";
+                for (var i = 0; i < newWord.length; i++) {
+                    dashString = dashString + "_";
+                }
+
+                /*  if you run out of guesses, you lose. reset*/
+            } else if (guesses == 0) {
+                wins = 0;
+                guesses = 10;
+                letters = [];
+
+                wordnumber = Math.floor(Math.random() * wordlist.length);
+                newWord = wordlist[wordnumber];
+                path = pathlist[wordnumber];
+                clue = cluelist[wordnumber];
+
+                dashString = "";
+                for (var i = 0; i < newWord.length; i++) {
+                    dashString = dashString + "_";
+                }
+            }
+
+            /* Update game board. */
+            document.getElementById("clue").innerHTML = "Clue: " + clue;
+            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            document.getElementById("guesses").innerHTML = "Guesses Remaining: " + guesses;
+            document.getElementById("word").innerHTML = dashString;
+            document.getElementById("letters").innerHTML = "Letters guessed: " + letters;
+        }
     }
 };
